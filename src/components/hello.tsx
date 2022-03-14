9ac908a2d746f4e37a75fff32dfe1eefe8fc3770
agagnon <28638888+bokoblin-link@users.noreply.github.com>
2022-03-14 09:48:18 -0400

    Testing home and gh pages

---
+++ src/components/hello.tsx
@@ -0,0 +1,4 @@
+import { FC } from "react"
+
+const Hello: FC = () => <h1>Hello</h1>
+export default Hello
---
+++ src/pages/home.tsx
@@ -0,0 +1,4 @@
+import { FC } from "react"
+
+const Home: FC = () => <p>Home</p>
+export default Home
