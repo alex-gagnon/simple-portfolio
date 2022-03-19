1dea81718266ceea25db7e7141a3ccf36447f738
agagnon <28638888+bokoblin-link@users.noreply.github.com>
2022-03-18 20:25:49 -0400

    Update 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import './App.css';
 import Home from './pages/home';
 
+const fs = require('fs')
+
+const cert = fs.readFileSync('../ssl/alex-gagnon_com.csr')
+const key = fs.readFileSync('../ssl/alex-gagnon_com.key')
+
 function App() {
   return (
     <div className="App">
