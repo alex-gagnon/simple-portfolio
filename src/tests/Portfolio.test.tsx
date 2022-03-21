b4773875895634dc7952aa6e8ccc54a0fdb08cbb
alex-gagnon <alexgagnon227@gmail.com>
2022-03-21 00:28:06 -0400

    drop some tests. need to make some new ones

--- src/tests/Portfolio.test.tsx
+++ src/tests/Portfolio.test.tsx
@@ -7,15 +7,3 @@ import Portfolio from '../Portfolio';
 test('renders without crashing', () => {
   shallow(<Portfolio />)
 })
-
-test('renders Home page first', () => {
-  render(<Portfolio />);
-  const text = screen.getByText(/site under construction/i);
-  expect(text).toBeInTheDocument();
-});
-
-test('renders placeholder navbar', () => {
-  render(<Portfolio />)
-  const text = screen.getByText(/Navbar placeholder/i)
-  expect(text).toBeInTheDocument()
-})
